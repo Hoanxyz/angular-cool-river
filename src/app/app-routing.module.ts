3d6d191169d710185bb65d75cbf0c3fd673a86e1
TuHT <tuht@arrowhitech.co>
2023-12-29 15:12:41 +0700

    Make styleguide-homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StyleguideComponent } from './styleguide/styleguide.component';
-
+import { HomeComponent } from './modules/cms/home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'styleguide', component: StyleguideComponent },
   {
     path: 'customer',
